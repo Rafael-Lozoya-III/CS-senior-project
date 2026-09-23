2c6543e24c7f6cb79ee39725b214f6ec406c7e37
Alfredo Zavala <fredzavala2020@gmail.com>
2026-09-22 20:36:15 -0500

    added home to make base for notifications

--- team_match/urls.py
+++ team_match/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from pages import views #Import views from app
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path('', views.home, name='home'),
+    ]
