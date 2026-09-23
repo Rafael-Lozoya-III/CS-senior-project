01e7d5fbf0e00ccdda5d504daed5a6ef1eecffd7
Jordan Gutierrez <jordan.gutierrez03@utrgv.edu>
2026-09-22 21:16:15 -0500

    Add student profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import StudentProfile
+
+
+@login_required
+def profile(request):
+    student_profile = StudentProfile.objects.get(user=request.user)
+
+    return render(
+        request,
+        "accounts/profile.html",
+        {"student_profile": student_profile},
+    )
